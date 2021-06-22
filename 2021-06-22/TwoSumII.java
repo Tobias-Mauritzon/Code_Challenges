644bad0f2a4151e22fc6aea9a374248571ac4a15
Tobias-Mauritzon <tobmau@student.chalmers.se>
2021-06-22 16:01:10 +0200

    Update TwoSumII.java

--- 2021-06-22/TwoSumII.java
+++ 2021-06-22/TwoSumII.java
@@ -29,4 +29,18 @@ public class TwoSumII {
 
         return ret;
     }
+	
+	// Great solution
+    public static int[] twoSum2(int[] numbers, int target) {
+        int n = numbers.length;
+        int i=0, j=n-1;
+        while(i<j)
+        {
+            if(numbers[i]+numbers[j]>target) j--;
+            else if(numbers[i]+numbers[j]<target) i++;
+            else return new int[]{i+1,j+1};
+            
+        }
+        return new int[]{};
+    }
 }
