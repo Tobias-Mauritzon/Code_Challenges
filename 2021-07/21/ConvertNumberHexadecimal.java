6fe6217bcbd627b96aa12c000b1de48b18a40b91
Tobias-Mauritzon <tobmau@student.chalmers.se>
2021-07-21 09:08:59 +0200

    Create ConvertNumberHexadecimal.java

---
+++ 2021-07/21/ConvertNumberHexadecimal.java
@@ -0,0 +1,11 @@
+
+/**
+ * Given an integer num, return a string representing its hexadecimal representation. For negative integers, two’s complement method is used.
+ * All the letters in the answer string should be lowercase characters, and there should not be any leading zeros in the answer except for the zero itself.
+ */
+
+public class ConvertNumberHexadecimal {
+    public String toHex(int num) {
+        return Integer.toHexString(num);
+    }
+}
