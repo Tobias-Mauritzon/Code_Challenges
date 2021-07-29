6b1ed39bb4a7267026638dddbd03931b1a62157e
tobias-mauritzon <tobmau@student.chalmers.se>
2021-07-29 10:42:06 +0200

    added method for if the  class is used on the same system

--- 2021-07/29/NextGreaterElement.java
+++ 2021-07/29/NextGreaterElement.java
@@ -12,8 +12,31 @@
  */
 public class NextGreaterElement {
 
+    //If the method is used on the same system then changing the original array
+    //could be bad and cause errors later in code;
+    //that is why method 2 was made
+    public int[] nextGreaterElement2(int[] nums1, int[] nums2) {
+        int[] arr = new int[nums1.length];
+        for(int i = 0; i < nums1.length; i++){
+            int num = nums1[i];
+            boolean found = false;
+            for(int j = 0; j < nums2.length; j++){
+                if(num == nums2[j])found = true;
+                
+                if(found){
+                    if(nums2[j] > num){
+                        arr[i] = nums2[j];
+                        found = false;
+                        break;
+                    }
+                }
+            }
+            if(found)arr[i] = -1; 
+        }
+        return nums1;
+    }
+    
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-
         for(int i = 0; i < nums1.length; i++){
             int num = nums1[i];
             boolean found = false;
